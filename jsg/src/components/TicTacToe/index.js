4bc0270d7161db23a46c8967131713c0f0485dc4
Andy <a.garcia.atn31@gmail.com>
2023-05-02 01:48:07 -0700

    Finish TicTacToe completely, Added a tally score, restart button, and styling

--- jsg/src/components/TicTacToe/index.js
+++ jsg/src/components/TicTacToe/index.js
@@ -1,48 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const TicTacToe = () => {
+function TicTacToe() {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
-  const winner = calculateWinner(board);
+  const [player, setPlayer] = useState("X");
+  const [status, setStatus] = useState("Next player: X");
+  const [xWins, setXWins] = useState(0);
+  const [oWins, setOWins] = useState(0);
 
-  const handleClick = (i) => {
+  function handleClick(index) {
+    // If there is already a winner or the square is already occupied, do nothing
+    if (calculateWinner(board) || board[index]) {
+      return;
+    }
+
     const newBoard = [...board];
-    if (winner || newBoard[i]) return;
-    newBoard[i] = isXNext ? 'X' : 'O';
+    newBoard[index] = player;
     setBoard(newBoard);
-    setIsXNext(!isXNext);
-  };
+    setPlayer(player === "X" ? "O" : "X");
 
-  const renderSquare = (i) => {
-    return (
-      <button className="border-2 border-gray-500 p-4 text-4xl font-bold" onClick={() => handleClick(i)}>
-        {board[i]}
-      </button>
-    );
-  };
-
-  let status;
-  if (winner) {
-    status = `Winner: ${winner}`;
-  } else if (board.every((square) => square !== null)) {
-    status = 'Draw';
-  } else {
-    status = `Next player: ${isXNext ? 'X' : 'O'}`;
+    // Check for winner after each move
+    const winner = calculateWinner(newBoard);
+    if (winner) {
+      setStatus(`Winner: ${winner}`);
+      if (winner === "X") {
+        setXWins(xWins + 1);
+      } else {
+        setOWins(oWins + 1);
+      }
+    } else {
+      setStatus(`Next player: ${player}`);
+    }
   }
 
+  function handleRestart() {
+    setBoard(Array(9).fill(null));
+    setPlayer("X");
+    setStatus("Next player: X");
+  }
+
+  function renderSquare(index) {
+    const squareValue = board[index];
+    let bgColor = "bg-green-500";
+    if (squareValue === "X") {
+      bgColor = "bg-blue-600";
+    } else if (squareValue === "O") {
+      bgColor = "bg-red-600";
+    }
+    return (
+      <div className="flex justify-center items-center">
+        <button
+          className={` ${bgColor} hover:bg-gray-800 font-bold py-48 px-48 rounded`}
+          onClick={() => handleClick(index)}
+        >
+          <span style={{ fontSize: `36px`, color: "white" }}>{squareValue}</span>
+        </button>
+      </div>
+    );
+  }
+
+  const winner = calculateWinner(board);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-4xl font-bold mb-4">{status}</div>
-      <div className="grid grid-cols-3 gap-4">
-        {board.map((square, index) => (
-          <div key={index}>{renderSquare(index)}</div>
-        ))}
+    <div>
+<div style={{ backgroundColor: "#262626" }}>
+  <div className="grid grid-cols-3 gap-1 p-12">
+    <div className="col-span-3 text-center font-bold">
+      <h1 className="text-4xl text-white mb-4" style={{ fontFamily: "Arial", fontSize:"96px" }}>Tic Tac Toe</h1>
+    </div>
+
+    <div className="col-span-3 text-center text-white font-bold">
+      <div className="text-3xl mb-8" style={{ marginTop: "25px" }}>
+        {winner ? `Winner: ${winner}` : <span style={{ fontFamily: "Arial", fontSize:"48px" }}>Next player: {player}</span>}
+      </div>
+      <div className="text-lg mb-4" style={{ fontFamily: "Arial", fontSize:"28px", marginTop: "5px" }}>
+        X Wins: {xWins} | O Wins: {oWins}
       </div>
     </div>
-  );
-};
+  </div>
+</div>
 
-const calculateWinner = (board) => {
+
+  
+      <div className={`grid grid-cols-3 gap-4`} style={{ backgroundColor: "#A9A3A3", margin: "5px" }}>
+        {board.map((square, index) => (
+          <div key={index} className="col-span-1">
+            {renderSquare(index)}
+          </div>
+        ))}
+  
+        <div className="col-span-3 text-center font-bold text-lg mt-4">
+          <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+      </div>
+    </div>
+  );    
+}
+
+
+function calculateWinner(board) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -60,9 +117,6 @@ const calculateWinner = (board) => {
     }
   }
   return null;
-};
+}
 
 export default TicTacToe;
-
-
-
