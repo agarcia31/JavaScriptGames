c636a7354edf36064558d141fa6f184d926ac7f8
Andy <a.garcia.atn31@gmail.com>
2023-05-02 19:56:34 -0700

    Set up the algorithm for connectFour

--- jsg/src/components/ConnectFour/index.js
+++ jsg/src/components/ConnectFour/index.js
@@ -1,29 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ConnectFour = () => {
-  const [board, setBoard] = useState(Array(6).fill(Array(7).fill(null)));
-  const [player, setPlayer] = useState('red');
+  const [board, setBoard] = useState(
+    Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null))
+  );
+  const [player, setPlayer] = useState("red");
 
   const dropPiece = (colIndex) => {
     const newBoard = [...board];
     for (let i = newBoard.length - 1; i >= 0; i--) {
       if (newBoard[i][colIndex] === null) {
         newBoard[i][colIndex] = player;
-        break;
+        setBoard(newBoard);
+        setPlayer(player === "red" ? "yellow" : "red");
+        return; // Stop the loop once the piece has been placed
       }
     }
-    setBoard(newBoard);
-    setPlayer(player === 'red' ? 'yellow' : 'red');
   };
 
   const checkWinner = () => {
-    // TODO: Implement check for winner
+    // Check for horizontal wins
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col < board[row].length - 3; col++) {
+        if (
+          board[row][col] &&
+          board[row][col] === board[row][col + 1] &&
+          board[row][col] === board[row][col + 2] &&
+          board[row][col] === board[row][col + 3]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check for vertical wins
+    for (let col = 0; col < board[0].length; col++) {
+      for (let row = 0; row < board.length - 3; row++) {
+        if (
+          board[row][col] &&
+          board[row][col] === board[row + 1][col] &&
+          board[row][col] === board[row + 2][col] &&
+          board[row][col] === board[row + 3][col]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check for diagonal wins (top-left to bottom-right)
+    for (let row = 0; row < board.length - 3; row++) {
+      for (let col = 0; col < board[row].length - 3; col++) {
+        if (
+          board[row][col] &&
+          board[row][col] === board[row + 1][col + 1] &&
+          board[row][col] === board[row + 2][col + 2] &&
+          board[row][col] === board[row + 3][col + 3]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // Check for diagonal wins (bottom-left to top-right)
+    for (let row = board.length - 1; row >= 3; row--) {
+      for (let col = 0; col < board[row].length - 3; col++) {
+        if (
+          board[row][col] &&
+          board[row][col] === board[row - 1][col + 1] &&
+          board[row][col] === board[row - 2][col + 2] &&
+          board[row][col] === board[row - 3][col + 3]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    // No winner found
     return false;
   };
 
   const resetBoard = () => {
-    setBoard(Array(6).fill(Array(7).fill(null)));
-    setPlayer('red');
+    setBoard(Array.from({ length: 6 }, () => Array(7).fill(null)));
+    setPlayer("red");
   };
 
   return (
@@ -34,13 +92,19 @@ const ConnectFour = () => {
           row.map((cell, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              className={`h-12 w-12 rounded-full ${cell === 'red' ? 'bg-red-500' : cell === 'yellow' ? 'bg-yellow-500' : ''} cursor-pointer`}
+              className={`h-12 w-12 rounded-full ${
+                cell === "red"
+                  ? "bg-red-500"
+                  : cell === "yellow"
+                  ? "bg-yellow-500"
+                  : ""
+              } cursor-pointer`}
               onClick={() => dropPiece(colIndex)}
             />
           ))
         )}
       </div>
-      <div className="mt-4">
+      <div>
         {checkWinner() && (
           <p className="text-lg font-bold text-green-500">{`${player.toUpperCase()} wins!`}</p>
         )}
@@ -54,9 +118,9 @@ const ConnectFour = () => {
       >
         Reset
       </button>
+      <div className="mt-4"></div>
     </div>
   );
 };
 
 export default ConnectFour;
-
